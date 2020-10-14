78fc171df274345c059b2f3a27440dc869fb72b2
Yosh Warrington <yjwarrington@gmail.com>
2020-10-14 21:37:10 +0100

    Initial commit

---
+++ WebAPI/Models/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class DBContext:DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
+
